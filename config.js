f9d4556dbe24f448adc852e1d7af5619c604a82b
andyparker1968-coder <andyparker1968@yourpocketapps.com>
2026-09-22 14:29:30 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
  * Market-data credentials, if ever needed, belong only in Netlify's
  * environment variables, never here.
  */
-window.POCKET_ISA_API_BASE = 'https://pocket-isa.netlify.app';
+window.POCKET_ISA_API_BASE = 'https://yahoo-proxy.andyparker1968.workers.dev';
